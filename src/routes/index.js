d70f0b2f0b41ddba8205946e4e52d7a9bb9fe1e9
manumac86 <emmartinez86@gmail.com>
2020-09-25 13:45:46 -0300

    Add Postgres connections

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { indexPage, userPage } from '../controllers';
+import { indexPage, userPage, messagesPage } from '../controllers';
 
 const indexRouter = express.Router();
 
 indexRouter.get('/', indexPage);
 indexRouter.get('/users', userPage);
+indexRouter.get('/messages', messagesPage);
 
 export default indexRouter;
