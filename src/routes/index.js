33e1085d25b279bbd70edecb01ac32331b87a0fb
manumac86 <emmartinez86@gmail.com>
2020-09-25 16:42:08 -0300

    Add POST to DB methods

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,16 @@
 import express from 'express';
-import { indexPage, userPage, messagesPage } from '../controllers';
+import {
+  indexPage,
+  userPage,
+  messagesPage,
+  addMessage,
+} from '../controllers';
 
 const indexRouter = express.Router();
 
 indexRouter.get('/', indexPage);
 indexRouter.get('/users', userPage);
 indexRouter.get('/messages', messagesPage);
+indexRouter.post('/messages', addMessage);
 
 export default indexRouter;
